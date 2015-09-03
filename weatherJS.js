ae2be9167749aca9fc74b657da81af9150e60431
Samantha Degges <samanthadegges@gmail.com>
2015-09-03 09:16:09 -0700

    initaited a validation within successful response to check for invalid zips. Error message not displaying but validation works.

--- weatherJS.js
+++ weatherJS.js
@@ -6,16 +6,13 @@ function init() {
   console.log('init works.');
   $('button').click(clicked);
 }
-//var navCapability = navigator.geolocation;
+
 var promiseDefaultLoc = $.getJSON("http://api.wunderground.com/api/0a5af13171ab6bbf/geolookup/q/autoip.json");
+
 promiseDefaultLoc.success(function(data) {
-    console.log('Default Loc is available and is: ',data.location.zip, data);
-  // navigator.geolocation.getCurrentPosition(function(position) {
-  //   console.log('position is: '+position);
-  //   console.log('testA');
-      //var cords = position.coords.latitude, position.coords.longitude;
-      $("input.form-control#zipCode").val(data.location.zip);
-      console.log('zip shud be replaced.');
+  console.log('Default Loc is available and is: ',data.location.zip, data);
+  $("input.form-control#zipCode").val(data.location.zip);
+  console.log('zip shud be replaced.');
 });
 
 function clicked() {
@@ -28,12 +25,16 @@ function clicked() {
     var promise = $.getJSON("http://api.wunderground.com/api/0a5af13171ab6bbf/geolookup/conditions/forecast/q/UnitedStates"+$zip+".json");
 
     promise.success(function(data) {
-      $("h4").removeClass("hidden");
-      $("#temperature").text(data.current_observation.feelslike_string);
-      $("#high").text(data.forecast.simpleforecast.forecastday[0].high.fahrenheit);
-      $("#weather").text(data.current_observation.display_location.full);
-      var imgURL = data.forecast.simpleforecast.forecastday[0].icon_url;
-      $("img").attr("src", imgURL);
+      if (data.current_observation.feelslike_string) {
+        $("h4").removeClass("hidden");
+        $("#temperature").text(data.current_observation.feelslike_string);
+        $("#high").text(data.forecast.simpleforecast.forecastday[0].high.fahrenheit);
+        $("#weather").text(data.current_observation.display_location.full);
+        var imgURL = data.forecast.simpleforecast.forecastday[0].icon_url;
+        $("img").attr("src", imgURL);
+      } else {
+        $("#weather").text("Incorrect Zip.");
+      }
     });
     promise.fail(function(error) {
       $("#weather").text("Try again.");
