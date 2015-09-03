1393d9805f0349f6afbe342be0e7864e10f5067f
Samantha Degges <samanthadegges@gmail.com>
2015-09-03 00:25:38 -0700

    request is not sent if the zip code entered is not 5 characters long.

--- weatherJS.js
+++ weatherJS.js
@@ -4,39 +4,33 @@ $(document).ready(init);
 
 function init() {
   console.log('init works.');
-  //$("").on('keypress', inputKeypress); Would this ruin mobile responsive?
-  $('button').click(goClicked);
+  $('button').click(clicked);
 }
 
-function goClicked() {
+function clicked() {
   console.log("Button clicked");
 
   var zip = $("input.form-control#zipCode").val();
-  console.log('zip is: ',zip);
+  console.log('length is: '+zip.length);
+  if (zip.length !== 5) {
+   $("#weather").text("Incorrect Zip.");
+ } else {
+    var promise = $.getJSON("http://api.wunderground.com/api/0a5af13171ab6bbf/geolookup/conditions/forecast/q/UnitedStates"+zip+".json");
 
-
-  var promise = $.getJSON("http://api.wunderground.com/api/0a5af13171ab6bbf/geolookup/conditions/forecast/q/UnitedStates"+zip+".json");
-
-  promise.success(function(data) {
-
-    var results = data;
-    console.log('success.');
-    $("h4").removeClass("hidden");
-    $("#temperature").text(results.current_observation.feelslike_string);
-    $("#high").text(results.forecast.simpleforecast.forecastday[0].high.fahrenheit);
-    $("#weather").text(results.current_observation.display_location.full);
-    var imgURL = results.forecast.simpleforecast.forecastday[0].icon_url;
-
-    console.log('imgURL is: ',imgURL);
-    $("img").attr("src", imgURL);
-
-  });
-  promise.fail(function(error) {
-    $("#location").text("try again.");
-    console.log('error is: ', error);
-  });
-  promise.always(function(data) {
-    //place anything you want to do after this request here. Especially if it's something you want to do, regardless of the request success or failure.
-    console.log('always data: ', data);
-  });
+    promise.success(function(data) {
+      $("h4").removeClass("hidden");
+      $("#temperature").text(data.current_observation.feelslike_string);
+      $("#high").text(data.forecast.simpleforecast.forecastday[0].high.fahrenheit);
+      $("#weather").text(data.current_observation.display_location.full);
+      var imgURL = data.forecast.simpleforecast.forecastday[0].icon_url;
+      $("img").attr("src", imgURL);
+    });
+    promise.fail(function(error) {
+      $("#weather").text("Try again.");
+      console.log('error is: ', error);
+    });
+    promise.always(function(data) {
+      console.log('always data: ', data);
+    });
+  }
 }
